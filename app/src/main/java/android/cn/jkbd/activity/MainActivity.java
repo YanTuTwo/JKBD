bdb20e5128d56c466286e9951ccd13e648827129
zjj <454550789@qq.com>
2017-06-28 21:40:39 +0800

    添加随机测试的方法

--- app/src/main/java/android/cn/jkbd/activity/MainActivity.java
+++ app/src/main/java/android/cn/jkbd/activity/MainActivity.java
@@ -3,6 +3,7 @@ package android.cn.jkbd.activity;
 import android.cn.jkbd.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void test(View view) {
     }
 }
