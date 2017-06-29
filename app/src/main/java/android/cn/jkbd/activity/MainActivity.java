903ac673bcb586c3b2a00ac711fbfc39dbf73147
zzz <454550789@qq.com>
2017-06-29 16:35:40 +0800

    随机测试获取网络数据考场信息输出到logcat

--- app/src/main/java/android/cn/jkbd/activity/MainActivity.java
+++ app/src/main/java/android/cn/jkbd/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package android.cn.jkbd.activity;
 
 import android.cn.jkbd.R;
+import android.cn.jkbd.bean.ExamInfo;
+import android.cn.jkbd.until.OkHttpUtils;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +19,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
-        Intent intent=new Intent(MainActivity.this,ExamActivity.class);
-        startActivity(intent);
+        OkHttpUtils<ExamInfo>until=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+        until.url(uri)
+                .targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
 
     public void exit(View view) {
